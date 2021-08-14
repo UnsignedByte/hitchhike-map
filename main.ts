60bee7d1dd5ecd6682b7b49dcf5815bcccd16ebb
UnsignedByte <edlam1889@gmail.com>
2021-08-14 15:08:38 -0700

    a

--- main.ts
+++ main.ts
@@ -130,7 +130,7 @@ export async function init (
       'clear @a minecraft:written_book{title:"Quest Book"}',
       '# Give new quest book',
       `execute at @a run summon item ~ ~ ~ {NoGravity:1b,Age:2,PickupDelay:1,Tags:["quest_book_template"],Item:{id:"minecraft:written_book",Count:1b,tag:{title:"Quest Book",author:""}}}`,
-      `execute as @e[tag=quest_book_template] run data modify entity @s Item set from entity @e[tag=quest_book_template,limit=1] Item`
+      `execute as @e[tag=quest_book_template] run data modify entity @s Item set from entity @e[tag=quest_book,limit=1] Item`
     )
   )
 
