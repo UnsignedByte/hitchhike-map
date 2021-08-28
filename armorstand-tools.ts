3c7b4f7513a43ed39c065d8bb1e41acb3410cead
UnsignedByte <edlam1889@gmail.com>
2021-08-28 16:21:02 -0700

    a

--- armorstand-tools.ts
+++ armorstand-tools.ts
@@ -107,10 +107,10 @@ function simulate_pile(bounds: [number, number], count: number, slope: [number, 
 			mass: 1,
 			shape: new CANNON.Box(new CANNON.Vec3(s/2, s/2, s/32)),
 			material: itemMat,
-			position: new CANNON.Vec3(Math.random()*(bounds[0]-s)+s/2, (i+0.5)*s/16+10, Math.random()*(bounds[1]-s)+s/2),
-			quaternion: new CANNON.Quaternion().setFromEuler(- Math.PI/2, 0, 0)
-			// position: new CANNON.Vec3(bounds[0]/2, i+5,bounds[1]/2),
-			// quaternion: new CANNON.Quaternion().setFromEuler(2*Math.PI/count*i, 0, 0)
+			// position: new CANNON.Vec3(Math.random()*(bounds[0]-s)+s/2, (i+0.5)*s/16+10, Math.random()*(bounds[1]-s)+s/2),
+			// quaternion: new CANNON.Quaternion().setFromEuler(- Math.PI/2, 0, 0)
+			position: new CANNON.Vec3(0, i+5,0),
+			quaternion: new CANNON.Quaternion().setFromEuler(2*Math.PI/count*i, 0, 0)
 		})
 		// objects[i].position.set()
 		// console.log(objects[i].position)
@@ -120,14 +120,14 @@ function simulate_pile(bounds: [number, number], count: number, slope: [number, 
 
 	console.log(`Generated scene with ${count} objects... simulating`)
 
-	for(let i = 0; i < tps*duration; i++) {
-		world.step(1/tps);
-		if (i%(tps*duration/10) == 0) console.log(`Simulated Second ${i/tps}`)
-	}
+	// for(let i = 0; i < tps*duration; i++) {
+	// 	world.step(1/tps);
+	// 	if (i%(tps*duration/10) == 0) console.log(`Simulated Second ${i/tps}`)
+	// }
 	// for(let i = 0; i < count; i++) {
 	// 	console.log(objects[i].position, objects[i].quaternion)
 	// }
-	return objects.filter(x=>x.position.x > 0 && x.position.z > 0 && x.position.x < bounds[0] && x.position.z < bounds[1]).map(x=>{
+	return objects.map(x=>{
 		const t = new CANNON.Vec3();
 		x.quaternion.toEuler(t);
 		return {position:x.position, rotation: t.scale(180/Math.PI), quaternion:x.quaternion}
