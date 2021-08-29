304d2d2d04276d4516d6679b7c3f43211cd5a6ab
UnsignedByte <edlam1889@gmail.com>
2021-08-28 19:28:24 -0700

    a

--- armorstand-tools.ts
+++ armorstand-tools.ts
@@ -23,7 +23,7 @@ export function generate_pile(corner: [number, number, number], item: string, co
 
 	for(let i = 0; i < 10; i++) {
 		let ang = new CANNON.Vec3(Math.random()*2*Math.PI, Math.random()*2*Math.PI, Math.random()*2*Math.PI);
-		let q = new CANNON.Quaternion().setFromEuler(ang.x, ang.y, ang.z);
+		let q = new CANNON.Quaternion().setFromEuler(ang.z, ang.y, ang.x, 'ZYX');
 		let t = new CANNON.Vec3();
 		q.toEuler(t)
 		console.log(ang, toEuler(q), t)
