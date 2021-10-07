544f08819eec2f198e1c3008c2d1f1d451cdd073
UnsignedByte <edlam1889@gmail.com>
2021-10-07 12:53:01 -0700

    a

--- main.ts
+++ main.ts
@@ -242,7 +242,7 @@ export async function init (
   functions[`npc/detect_interact`] = [
     `scoreboard players set -detect-count npc-interact 0`,
     `execute at @s anchored eyes run function generated:npc/_detect_interact`,
-    `effect give @e[tag=player_facing_npc] glowing 1 0 true`,
+    // `effect give @e[tag=player_facing_npc] glowing 1 0 true`,
     `tag @e remove player_facing_npc`,
     `execute if entity @e[tag=selected_npc] run tag @s add npc_selector`,
   ]
