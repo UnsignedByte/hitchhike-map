b3b3f4f86b679b9a61fffbca80904d4aa9d2036c
UnsignedByte <edlam1889@gmail.com>
2021-10-07 11:28:44 -0700

    a

--- main.ts
+++ main.ts
@@ -241,7 +241,7 @@ export async function init (
   //tag npcs player looks towards
   functions[`npc/detect_interact`] = [
     `scoreboard players set -detect-count npc-interact 0`,
-    `execute at @s run function generated:npc/_detect_interact`,
+    `execute at @s anchored eyes run function generated:npc/_detect_interact`,
     `tag @e remove player_facing_npc`,
     `execute if entity @e[tag=selected_npc] run tag @s add npc_selector`
   ]
