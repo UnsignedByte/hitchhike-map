f2677eef6a2248564f0645226e25bc7183a4997b
UnsignedByte <edlam1889@gmail.com>
2021-10-11 21:29:55 -0700

    a

--- compile-to-mcfunction.ts
+++ compile-to-mcfunction.ts
@@ -1200,7 +1200,9 @@ export function story(functions: Record<string, Lines>, reset: Lines[], load: Li
       'scoreboard players operation #MIN maze-pathTcost < @e[type=marker,tag=maze-node,tag=path-activated] maze-pathTcost',
       '# visit all the minimum t cost cells',
       'execute as @e[type=marker,tag=maze-node,tag=path-activated] if score @s maze-pathTcost = #MIN maze-pathTcost at @s run function generated:story/maze/pathfind/visitcell',
-      'execute unless entity @e[type=marker,tag=maze-node,tag=path-goal,tag=path-activated] run schedule function generated:story/maze/pathfind/selectcell 1t'
+      'execute unless entity @e[type=marker,tag=maze-node,tag=path-goal,tag=path-activated] run schedule function generated:story/maze/pathfind/selectcell 1t',
+      '# Finalize path after finishing',
+      'execute unless entity @e[type=marker,tag=maze-node,tag=path-goal,tag=path-activated] run function generated:story/maze/pathfind/finalizepath'
     ])
 
     addfunc('maze/pathfind/visitcell', [
--- data/generated/functions/story/maze/pathfind/selectcell.mcfunction
+++ data/generated/functions/story/maze/pathfind/selectcell.mcfunction
@@ -4,3 +4,5 @@ scoreboard players operation #MIN maze-pathTcost < @e[type=marker,tag=maze-node,
 # visit all the minimum t cost cells
 execute as @e[type=marker,tag=maze-node,tag=path-activated] if score @s maze-pathTcost = #MIN maze-pathTcost at @s run function generated:story/maze/pathfind/visitcell
 execute unless entity @e[type=marker,tag=maze-node,tag=path-goal,tag=path-activated] run schedule function generated:story/maze/pathfind/selectcell 1t
+# Finalize path after finishing
+execute unless entity @e[type=marker,tag=maze-node,tag=path-goal,tag=path-activated] run function generated:story/maze/pathfind/finalizepath
