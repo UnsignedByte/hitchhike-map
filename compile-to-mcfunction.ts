ae81f4fb40e5186ab898caed27b261475f931891
UnsignedByte <edlam1889@gmail.com>
2021-10-11 22:23:29 -0700

    a

--- compile-to-mcfunction.ts
+++ compile-to-mcfunction.ts
@@ -1287,8 +1287,7 @@ export function story(functions: Record<string, Lines>, reset: Lines[], load: Li
     addfunc('maze/pathfind/finalizepath', [
       '#> Reverse the path to get from start to goal',
       '# First, clear the scoreboard',
-      'scoreboard objectives remove maze-path',
-      'scoreboard objectives add maze-path dummy',
+      'scoreboard objectives reset @e[tag=maze-node] maze-path',
       'scoreboard players set length maze-path 0',
       'execute as @e[type=marker,tag=maze-node,tag=path-goal] run function generated:story/maze/pathfind/_pathgetnext'
     ])
--- data/generated/functions/story/maze/pathfind/finalizepath.mcfunction
+++ data/generated/functions/story/maze/pathfind/finalizepath.mcfunction
@@ -1,6 +1,5 @@
 #> Reverse the path to get from start to goal
 # First, clear the scoreboard
-scoreboard objectives remove maze-path
-scoreboard objectives add maze-path dummy
+scoreboard objectives reset @e[tag=maze-node] maze-path
 scoreboard players set length maze-path 0
 execute as @e[type=marker,tag=maze-node,tag=path-goal] run function generated:story/maze/pathfind/_pathgetnext
