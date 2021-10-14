199f80f94d357923273577c2331609efc88fd774
UnsignedByte <edlam1889@gmail.com>
2021-10-13 17:19:13 -0700

    a

--- compile-to-mcfunction.ts
+++ compile-to-mcfunction.ts
@@ -1203,7 +1203,7 @@ export function story(functions: Record<string, Lines>, reset: Lines[], load: Li
 
 
     addfunc('maze/pathfind', [
-      // 'kill @e[tag=maze-path-lit]',
+      'kill @e[tag=maze-path-lit]',
       '# Cleanup',
       'schedule clear generated:story/maze/pathfind/selectcell',
       'tag @e[type=marker,tag=maze-node] remove path-visited',
@@ -1244,7 +1244,7 @@ export function story(functions: Record<string, Lines>, reset: Lines[], load: Li
     ])
 
     addfunc('maze/pathfind/visitcell', [
-      // 'execute at @s run team join blue @e[tag=maze-path-lit,sort=nearest,limit=1]',
+      'execute at @s run team join blue @e[tag=maze-path-lit,sort=nearest,limit=1]',
       '# This is now visited',
       'tag @s add path-visited',
       'tag @s remove path-activated',
@@ -1272,7 +1272,7 @@ export function story(functions: Record<string, Lines>, reset: Lines[], load: Li
     ])
 
     addfunc('maze/pathfind/getg', [
-      // 'execute at @s run summon minecraft:armor_stand ~ ~ ~ {NoGravity:1,NoAI:1,Glowing:1,Tags:["maze-path-lit"]}',
+      'execute at @s run summon minecraft:armor_stand ~ ~ ~ {NoGravity:1,NoAI:1,Glowing:1,Tags:["maze-path-lit"]}',
       '#> Get G cost of a given marker',
       'execute as @s run function generated:story/maze/getpos',
       '# Use euclidian distance',
@@ -1302,7 +1302,7 @@ export function story(functions: Record<string, Lines>, reset: Lines[], load: Li
     ])
 
     addfunc('maze/pathfind/_pathgetnext', [
-      // 'execute at @s run team join magenta @e[tag=maze-path-lit,sort=nearest,limit=1]',
+      'execute at @s run team join magenta @e[tag=maze-path-lit,sort=nearest,limit=1]',
       '#> Propogate through path recursively',
       'scoreboard players operation @s maze-path = length maze-path',
       'scoreboard players add length maze-path 1',
