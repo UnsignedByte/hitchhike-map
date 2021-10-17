40767ec63b4d57c052cd3783b3da856f0251e7a2
UnsignedByte <edlam1889@gmail.com>
2021-10-16 20:19:28 -0700

    a

--- compile-to-mcfunction.ts
+++ compile-to-mcfunction.ts
@@ -1106,7 +1106,8 @@ export function story(functions: Record<string, Lines>, reset: Lines[], load: Li
         `playsound entity.blaze.shoot hostile @a ~ ~1 ~ 1 1.2`
       ],
       [
-        `say hi`
+        `summon area_effect_cloud ~ ~ ~ {Particle:"entity_effect",ReapplicationDelay:10,Radius:0.5f,RadiusPerTick:0.05f,RadiusOnUse:0f,Duration:60,DurationOnUse:0f,Color:16711680,Effects:[{Id:20b,Amplifier:2b,Duration:20,ShowParticles:0b}]}`,
+        `data modify entity @s Motion[1] set value 1`
       ]
     ])
 
--- data/generated/functions/story/maze/mobs/move/ip/move-1.mcfunction
+++ data/generated/functions/story/maze/mobs/move/ip/move-1.mcfunction
@@ -1,1 +1,2 @@
-say hi
+summon area_effect_cloud ~ ~ ~ {Particle:"entity_effect",ReapplicationDelay:10,Radius:0.5f,RadiusPerTick:0.05f,RadiusOnUse:0f,Duration:60,DurationOnUse:0f,Color:16711680,Effects:[{Id:20b,Amplifier:2b,Duration:20,ShowParticles:0b}]}
+data modify entity @s Motion[1] set value 1
