1fcb977e04d142fc4bb3df6201ebdd56b84e3a28
UnsignedByte <edlam1889@gmail.com>
2021-10-17 19:39:57 -0700

    a

--- compile-to-mcfunction.ts
+++ compile-to-mcfunction.ts
@@ -1196,7 +1196,7 @@ export function story(functions: Record<string, Lines>, reset: Lines[], load: Li
       'execute store result score #CMP UUID2 run data get entity @s data.PlayerUUID[2]',
       'execute store result score #CMP UUID3 run data get entity @s data.PlayerUUID[3]',
       'tag @a add match-selectable',
-      Object.keys(weapons).map(k=>`execute if entity @s[tag=maze-weapon-${k}] as @a[tag=match-uuid-select] run function generated:maze/weapons/${k}/give`)
+      Object.keys(weapons).map(k=>`execute if entity @s[tag=maze-weapon-${k}] as @a[tag=match-uuid-select] run function generated:story/maze/weapons/${k}/give`)
     ])
 
 
--- data/generated/functions/story/maze/weapons/return.mcfunction
+++ data/generated/functions/story/maze/weapons/return.mcfunction
@@ -3,4 +3,4 @@ execute store result score #CMP UUID1 run data get entity @s data.PlayerUUID[1]
 execute store result score #CMP UUID2 run data get entity @s data.PlayerUUID[2]
 execute store result score #CMP UUID3 run data get entity @s data.PlayerUUID[3]
 tag @a add match-selectable
-execute if entity @s[tag=maze-weapon-spoon] as @a[tag=match-uuid-select] run function generated:maze/weapons/spoon/give
+execute if entity @s[tag=maze-weapon-spoon] as @a[tag=match-uuid-select] run function generated:story/maze/weapons/spoon/give
