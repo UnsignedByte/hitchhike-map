2dfc8500c28d485b2f73c72080f40ecb103b17df
UnsignedByte <edlam1889@gmail.com>
2021-10-22 13:38:31 -0700

    a

--- compile-to-mcfunction.ts
+++ compile-to-mcfunction.ts
@@ -1303,6 +1303,7 @@ export function story(functions: Record<string, Lines>, reset: Lines[], load: Li
 
     addfunc('maze/weapons/detecthit', [
       'execute as @s at @s run function hitchhike:hitdetect/detect',
+      'say @s',
       Object.entries(weapons).map(([k, v])=>`execute if entity @s[nbt={SelectedItem:${toSnbt(v)}}] as @s at @s run function hitchhike:story/maze/weapons/${k}/onhit`)
     ])
 
