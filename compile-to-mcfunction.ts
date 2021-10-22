37ec3d0d3b00202931473e0f6f27bfc40ff84642
UnsignedByte <edlam1889@gmail.com>
2021-10-21 18:02:33 -0700

    a

--- compile-to-mcfunction.ts
+++ compile-to-mcfunction.ts
@@ -1180,7 +1180,7 @@ export function story(functions: Record<string, Lines>, reset: Lines[], load: Li
         `scoreboard players set #rotX vars 0`,
         `scoreboard players set #rotY vars 0`,
         `scoreboard players set #rotZ vars 0`,
-        `execute at @e[tag=maze-mob,distance=0.01..6] positioned ~ ~3 ~ run function hitchhike:story/maze/mobs/johnny/anvil`,
+        `execute at @e[tag=maze-mob,distance=0.01..6,sort=random,limit=5] positioned ~ ~3 ~ run function hitchhike:story/maze/mobs/johnny/anvil`,
       ],
     ])
 
