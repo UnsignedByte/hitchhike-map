8de7f7e1bfbc23c195aa60155fc51f4f20b74db0
UnsignedByte <edlam1889@gmail.com>
2021-10-23 13:47:50 -0700

    a

--- compile-to-mcfunction.ts
+++ compile-to-mcfunction.ts
@@ -1428,6 +1428,7 @@ export function story(functions: Record<string, Lines>, reset: Lines[], load: Li
       'bossbar set minecraft:maze name [{"text":"Loading SD Card"}]',
       'bossbar set minecraft:maze color blue',
       'scoreboard players operation bossbar maze = size maze',
+      'scoreboard players operation bossbar maze *= size maze',
       'execute store result bossbar minecraft:maze max run scoreboard players get bossbar maze',
       'bossbar set minecraft:maze players @a',
       'bossbar set minecraft:maze visible true',
@@ -1435,7 +1436,6 @@ export function story(functions: Record<string, Lines>, reset: Lines[], load: Li
       'bossbar set minecraft:maze value 0',
       'scoreboard players set bossbar maze 0',
       'scoreboard players operation batchsize maze = size maze',
-      'scoreboard players operation batchsize maze *= size maze',
       '',
       'function generated:story/maze/create/_loadcellbatch'
     ])
