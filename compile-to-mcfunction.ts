430294c492730f489038af9f0ff7a35287b66c29
UnsignedByte <edlam1889@gmail.com>
2021-11-18 22:30:43 -0800

    b

--- compile-to-mcfunction.ts
+++ compile-to-mcfunction.ts
@@ -1135,7 +1135,7 @@ export function story(functions: Record<string, Lines>, reset: Lines[], load: Li
     ])
 
     addfunc('maze/mobs/boss/segfault/do_summon_arrows', [
-      'execute at at @e[tag=maze-boss-segfault,limit=1] positioned ~ ~1 ~ run playsound minecraft:entity.ghast.shoot hostile @a ~ ~ ~ 100 1.4',
+      'execute at @e[tag=maze-boss-segfault,limit=1] positioned ~ ~1 ~ run playsound minecraft:entity.ghast.shoot hostile @a ~ ~ ~ 100 1.4',
       `execute at @e[tag=maze-boss-segfault,limit=1] positioned ~ ~1 ~ run function generated:story/maze/mobs/boss/segfault/summon_arrows`
     ])
 
--- data/generated/functions/story/maze/mobs/boss/segfault/do_summon_arrows.mcfunction
+++ data/generated/functions/story/maze/mobs/boss/segfault/do_summon_arrows.mcfunction
@@ -1,2 +1,2 @@
-execute at at @e[tag=maze-boss-segfault,limit=1] positioned ~ ~1 ~ run playsound minecraft:entity.ghast.shoot hostile @a ~ ~ ~ 100 1.4
+execute at @e[tag=maze-boss-segfault,limit=1] positioned ~ ~1 ~ run playsound minecraft:entity.ghast.shoot hostile @a ~ ~ ~ 100 1.4
 execute at @e[tag=maze-boss-segfault,limit=1] positioned ~ ~1 ~ run function generated:story/maze/mobs/boss/segfault/summon_arrows
