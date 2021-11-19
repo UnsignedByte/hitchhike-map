6743394b777688ffb6b1a356de7afd93aab4a491
UnsignedByte <edlam1889@gmail.com>
2021-11-18 22:29:52 -0800

    b

--- compile-to-mcfunction.ts
+++ compile-to-mcfunction.ts
@@ -1134,7 +1134,7 @@ export function story(functions: Record<string, Lines>, reset: Lines[], load: Li
       `schedule function generated:story/maze/mobs/boss/segfault/do_summon_arrows 20t`
     ])
 
-    addfunc('maze/mobs/segfault/do_summon_arrows', [
+    addfunc('maze/mobs/boss/segfault/do_summon_arrows', [
       'execute at at @e[tag=maze-boss-segfault,limit=1] positioned ~ ~1 ~ run playsound minecraft:entity.ghast.shoot hostile @a ~ ~ ~ 100 1.4',
       `execute at @e[tag=maze-boss-segfault,limit=1] positioned ~ ~1 ~ run function generated:story/maze/mobs/boss/segfault/summon_arrows`
     ])
