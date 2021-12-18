29f51dfa453ca50ee810780bbdb49f97091c97d9
UnsignedByte <edlam1889@gmail.com>
2021-12-17 21:14:44 -0800

    b

--- compile-to-mcfunction.ts
+++ compile-to-mcfunction.ts
@@ -1298,6 +1298,10 @@ export function story(functions: Record<string, Lines>, reset: Lines[], load: Li
       `scoreboard players operation #tmp maze-weapon-age = @s maze-weapon-age`,
       `scoreboard players operation #tmp maze-weapon-age %= 10 const`,
       `execute if score #tmp maze-weapon-age matches 0 run tp @s ^ ^ ^-5`,
+      `execute if score #tmp maze-weapon-age matches 0 if score @s maze-weapon-age matches 0..9 run playsound minecraft:entity.shulker.teleport hostile @a ~ ~ ~ 1 0.3`,
+      `execute if score #tmp maze-weapon-age matches 0 if score @s maze-weapon-age matches 10..19 run playsound minecraft:entity.shulker.teleport hostile @a ~ ~ ~ 1 0.6`,
+      `execute if score #tmp maze-weapon-age matches 0 if score @s maze-weapon-age matches 20..29 run playsound minecraft:entity.shulker.teleport hostile @a ~ ~ ~ 1 0.9`,
+      `execute if score #tmp maze-weapon-age matches 0 if score @s maze-weapon-age matches 30..39 run playsound minecraft:entity.shulker.teleport hostile @a ~ ~ ~ 1 1.2`,
       'execute if score @s maze-weapon-age matches 40.. run kill @s'
     ])
 
