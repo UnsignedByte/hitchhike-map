fe4a74f98e59d9c4b107354e6478db136aec7caa
UnsignedByte <edlam1889@gmail.com>
2021-12-23 17:48:00 -0800

    b

--- compile-to-mcfunction.ts
+++ compile-to-mcfunction.ts
@@ -1878,7 +1878,7 @@ export function story(functions: Record<string, Lines>, reset: Lines[], load: Li
       '',
       'execute as @e[type=marker,tag=maze-marker,tag=maze-create-root] at @s run function generated:story/maze/create/_y',
       'scoreboard players remove _x maze 1',
-      `execute as @e[type=marker,tag=maze-marker,tag=maze-create-root] at @s run tp ~${cellsize} ${(cellsize-1)/2} 0`,
+      `execute as @e[type=marker,tag=maze-marker,tag=maze-create-root] at @s run tp ~${cellsize} ${cellsize+(cellsize-1)/2} 0`,
       'execute unless score _x maze matches 0 run schedule function generated:story/maze/create/_x 1t',
       'execute if score _x maze matches 0 run function generated:story/maze/create/_generatepaths'
     ]);
--- data/generated/functions/story/maze/create/_x.mcfunction
+++ data/generated/functions/story/maze/create/_x.mcfunction
@@ -4,6 +4,6 @@ execute store result bossbar minecraft:maze value run scoreboard players get bos
 
 execute as @e[type=marker,tag=maze-marker,tag=maze-create-root] at @s run function generated:story/maze/create/_y
 scoreboard players remove _x maze 1
-execute as @e[type=marker,tag=maze-marker,tag=maze-create-root] at @s run tp ~9 4 0
+execute as @e[type=marker,tag=maze-marker,tag=maze-create-root] at @s run tp ~9 13 0
 execute unless score _x maze matches 0 run schedule function generated:story/maze/create/_x 1t
 execute if score _x maze matches 0 run function generated:story/maze/create/_generatepaths
