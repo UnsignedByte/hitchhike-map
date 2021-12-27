81755ace1555a0f8ee8ebe06a69cc6988eb40e94
UnsignedByte <edlam1889@gmail.com>
2021-12-26 22:59:23 -0800

    b

--- compile-to-mcfunction.ts
+++ compile-to-mcfunction.ts
@@ -2458,10 +2458,7 @@ export function story(functions: Record<string, Lines>, reset: Lines[], load: Li
     addfunc('fountain/jar/updateguess', [
       `execute positioned 914.5 49 -72.5 as @e[type=item,distance=..1,nbt={Item:${toSnbt(item.btc)}}] run function generated:story/fountain/jar/_updateguess`,
       'scoreboard players operation g_B fishjar = guesscount fishjar',
-      'scoreboard players operation g_B fishjar /= 100 const',
-      'scoreboard players operation g_b fishjar = guesscount fishjar',
-      'scoreboard players operation g_b fishjar %= 100 const',
-      'data modify entity @e[tag=fishjar-guess-display,limit=1] CustomName set value [{"text":"Current Guess: ","color":"gold","bold":true},{"score":{"name":"g_B","objective":"fishjar"}},{"text":"."},{"score":{"name":"g_b","objective":"fishjar"}},{"text":" Bov"}]',
+      `data modify entity @e[tag=fishjar-guess-display,limit=1] CustomName set value '[{"text":"Current Guess: ","color":"gold","bold":true},{"score":{"name":"guesscount","objective":"fishjar"}},{"text":" Bov"}]'`,
       'execute positioned 914.5 49 -72.5 run tp @e[type=item,distance=..1] 914.5 55 -73.5'
     ])
 
