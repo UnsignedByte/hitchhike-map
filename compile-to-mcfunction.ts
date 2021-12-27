485242f75e90311a3e6c7899fffd5b8547356fcc
UnsignedByte <edlam1889@gmail.com>
2021-12-26 23:17:02 -0800

    b

--- compile-to-mcfunction.ts
+++ compile-to-mcfunction.ts
@@ -2457,7 +2457,7 @@ export function story(functions: Record<string, Lines>, reset: Lines[], load: Li
 
     addfunc('fountain/jar/updateguess', [
       `execute positioned 914.5 49 -72.5 as @e[type=item,distance=..1,nbt={Item:${toSnbt(item.btc)}}] run function generated:story/fountain/jar/_updateguess`,
-      `data modify block 914 56 -74 Text3 set value '[{"color":"gold","score":{"name":"guesscount","objective":"fishjar"}},{"text":" Bov"}]''`,
+      `data modify block 914 56 -74 Text3 set value '[{"color":"gold","score":{"name":"guesscount","objective":"fishjar"}},{"text":" Bov"}]'`,
       'execute positioned 914.5 49 -72.5 run tp @e[type=item,distance=..1] 914.5 55 -73.5'
     ])
 
--- data/generated/functions/story/fountain/jar/updateguess.mcfunction
+++ data/generated/functions/story/fountain/jar/updateguess.mcfunction
@@ -1,3 +1,3 @@
 execute positioned 914.5 49 -72.5 as @e[type=item,distance=..1,nbt={Item:{id: "minecraft:sunflower", tag: {display: {Name: '{"text":"1 BTC","color":"gold"}', Lore: ['{"text":"Valued at 1.00 BOV"}']}}}}] run function generated:story/fountain/jar/_updateguess
-data modify block 914 56 -74 Text3 set value '[{"color":"gold","score":{"name":"guesscount","objective":"fishjar"}},{"text":" Bov"}]''
+data modify block 914 56 -74 Text3 set value '[{"color":"gold","score":{"name":"guesscount","objective":"fishjar"}},{"text":" Bov"}]'
 execute positioned 914.5 49 -72.5 run tp @e[type=item,distance=..1] 914.5 55 -73.5
