04cea5989effe3290e6b22eccaec92326e7b2016
UnsignedByte <edlam1889@gmail.com>
2021-12-29 09:24:12 -0800

    b

--- compile-to-mcfunction.ts
+++ compile-to-mcfunction.ts
@@ -2725,7 +2725,7 @@ export function story(functions: Record<string, Lines>, reset: Lines[], load: Li
     addfunc('lake/piranha/summon', [
       `summon drowned ~ ~ ~ {Silent:1b,Invulnerable:1b,CustomNameVisible:0b,IsBaby:1b,Tags:["invisible","lake-piranha","lake-piranha-init"],CustomName:'{"text":"piranha","color":"red"}',Attributes:[{Name:generic.follow_range,Base:30},{Name:generic.movement_speed,Base:1},{Name:generic.attack_damage,Base:10},{Name:zombie.spawn_reinforcements,Base:0}]}`,
       'summon tropical_fish ~ ~ ~ {Invulnerable:1b,Tags:["lake-piranha","lake-piranha-init"],Variant:118358272}',
-      'scoreboard players operation @e[tag=lake-piranha-init piranha-id = max piranha-id',
+      'scoreboard players operation @e[tag=lake-piranha-init] piranha-id = max piranha-id',
       'scoreboard players add max piranha-id 1',
       'tag @e remove lake-piranha-init'
     ])
