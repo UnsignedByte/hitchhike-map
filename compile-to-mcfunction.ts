e2400d56695c0d26a36033919e4f1e2e065f65d7
UnsignedByte <edlam1889@gmail.com>
2022-01-07 16:39:58 -0800

    b

--- compile-to-mcfunction.ts
+++ compile-to-mcfunction.ts
@@ -822,8 +822,8 @@ export function story(functions: Record<string, Lines>, reset: Lines[], load: Li
     ])
 
     addfunc('intro/close_back_door', [
-      `execute unless block 1007 65 65 minecraft:dark_oak_door[facing=north,half=lower,hinge=right,open=false,powered=false] run title @a actionbar [{"text":"The back door is locked. Maybe look for a key?"}]`,
-      `execute unless block 1007 65 65 minecraft:dark_oak_door[facing=north,half=lower,hinge=right,open=false,powered=false] run setblock 1009 64 65 minecraft:redstone_block`,
+      `execute unless block 1007 65 65 minecraft:dark_oak_door[facing=south,half=lower,hinge=left,open=false,powered=false] run title @a actionbar [{"text":"The back door is locked. Maybe look for a key?"}]`,
+      `execute unless block 1007 65 65 minecraft:dark_oak_door[facing=south,half=lower,hinge=left,open=false,powered=false] run setblock 1009 64 65 minecraft:redstone_block`,
       `setblock 1009 64 65 air`
     ])
 
@@ -833,8 +833,8 @@ export function story(functions: Record<string, Lines>, reset: Lines[], load: Li
     ])
 
     addfunc('intro/close_front_door', [
-      `execute unless block 1012 65 52 minecraft:dark_oak_door[facing=north,half=lower,hinge=right,open=false,powered=false] run title @a actionbar [{"text":"The front door is locked. Maybe look for a key?"}]`,
-      `execute unless block 1012 65 52 minecraft:dark_oak_door[facing=north,half=lower,hinge=right,open=false,powered=false] run setblock 1014 63 51 minecraft:redstone_block`,
+      `execute unless block 1012 65 52 minecraft:dark_oak_door[facing=north,half=lower,hinge=left,open=false,powered=false] run title @a actionbar [{"text":"The front door is locked. Maybe look for a key?"}]`,
+      `execute unless block 1012 65 52 minecraft:dark_oak_door[facing=north,half=lower,hinge=left,open=false,powered=false] run setblock 1014 63 51 minecraft:redstone_block`,
       `setblock 1014 63 51 air`
     ])
 
--- data/generated/functions/story/intro/close_front_door.mcfunction
+++ data/generated/functions/story/intro/close_front_door.mcfunction
@@ -1,3 +1,3 @@
-execute unless block 1012 65 52 minecraft:dark_oak_door[facing=north,half=lower,hinge=right,open=false,powered=false] run title @a actionbar [{"text":"The front door is locked. Maybe look for a key?"}]
-execute unless block 1012 65 52 minecraft:dark_oak_door[facing=north,half=lower,hinge=right,open=false,powered=false] run setblock 1014 63 51 minecraft:redstone_block
+execute unless block 1012 65 52 minecraft:dark_oak_door[facing=north,half=lower,hinge=left,open=false,powered=false] run title @a actionbar [{"text":"The front door is locked. Maybe look for a key?"}]
+execute unless block 1012 65 52 minecraft:dark_oak_door[facing=north,half=lower,hinge=left,open=false,powered=false] run setblock 1014 63 51 minecraft:redstone_block
 setblock 1014 63 51 air
