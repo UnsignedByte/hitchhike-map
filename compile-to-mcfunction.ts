48a9ab2d6cf20117b1b09691b63f4fb105a49123
UnsignedByte <edlam1889@gmail.com>
2022-01-10 12:10:41 -0800

    b

--- compile-to-mcfunction.ts
+++ compile-to-mcfunction.ts
@@ -3443,7 +3443,7 @@ export function story(functions: Record<string, Lines>, reset: Lines[], load: Li
     addfunc('parkour/updatespawn', [
       `scoreboard players operation #tmp checkpoint-id = @s checkpoint-id`,
       `execute positioned ~ ~${yoffset} ~ run tag @a[dx=0,dy=0,dz=0] add checkpoint-candidate`,
-      `execute as @a[tag=checkpoint-candidate] if score @s checkpoint-id matches #tmp checkpoint-id run tag @s remove checkpoint-candidate`,
+      `execute as @a[tag=checkpoint-candidate] if score @s checkpoint-id = #tmp checkpoint-id run tag @s remove checkpoint-candidate`,
       `scoreboard players operation @a[tag=checkpoint-candidate] checkpoint-id = @s checkpoint-id`,
       `playsound minecraft:entity.player.levelup player @a[tag=checkpoint-candidate] ~ ~${yoffset} ~ 0.3`
     ])
