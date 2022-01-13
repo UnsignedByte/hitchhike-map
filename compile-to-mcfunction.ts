859e8cd0002ae900a65bb842175b0fca2363d8fc
UnsignedByte <edlam1889@gmail.com>
2022-01-13 14:13:52 -0800

    b

--- compile-to-mcfunction.ts
+++ compile-to-mcfunction.ts
@@ -2744,7 +2744,8 @@ export function story(functions: Record<string, Lines>, reset: Lines[], load: Li
       'scoreboard players operation #tmp piranha-id = @s piranha-id',
       'execute as @e[type=tropical_fish,tag=lake-piranha] if score @s piranha-id = #tmp piranha-id facing entity @p eyes run tag @s add lake-piranha-selected',
       'tp @e[tag=lake-piranha-selected] ~ ~ ~ ~ ~',
-      'execute unless entity @e[tag=lake-piranha-selected] as @s run function hitchhike:kill'
+      'execute unless entity @e[tag=lake-piranha-selected] as @s run function hitchhike:kill',
+      'tag @e remove lake-piranha-selected'
       // 'tp @e[tag=piranha-selected] @s',
       // 'tag @e remove piranha-selected'
     ])
