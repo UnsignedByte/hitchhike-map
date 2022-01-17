39b6abd96ce500c3b1c595b2903cbb20f4607615
UnsignedByte <edlam1889@gmail.com>
2022-01-16 21:42:01 -0800

    b

--- compile-to-mcfunction.ts
+++ compile-to-mcfunction.ts
@@ -889,12 +889,12 @@ export function story(functions: Record<string, Lines>, reset: Lines[], load: Li
     addfunc('intro/await_start', [
       'execute unless entity @a[nbt={SelectedItem:{tag:{tvremote:1b}}}] as @e[tag=remote_rightclick_detect] at @s run tp @s ~ 0 ~',
       'execute as @a[nbt={SelectedItem:{tag:{tvremote:1b}}}] at @s run function generated:story/intro/tp_remote_holder',
-      'execute as @e[tag=remote_rightclick_detect,nbt={HandItems:[{tag:{tvremote:1b}}]}] at @s run function generated:story/intro/start',
+      'execute as @e[tag=remote_rightclick_detect,nbt={HandItems:[{tag:{tvremote:1b}}]}] at @s run function generated:story/intro/_start',
       '',
       'execute unless score start story-intro matches 1 run schedule function generated:story/intro/await_start 1t'
     ])
 
-    addfunc('intro/start', [
+    addfunc('intro/_start', [
       `say @s`,
       `execute positioned ~ ~-0.7 ~ run give @p ${toGive(item.tvremote)}`,
       `kill @s`,
---
+++ data/generated/functions/story/intro/_start.mcfunction
@@ -0,0 +1,4 @@
+say @s
+execute positioned ~ ~-0.7 ~ run give @p minecraft:black_candle{Enchantments: [{}], display: {Lore: ['{"text":"Right click to play/pause.","color":"dark_gray"}'], Name: '{"text":"TV Remote","color":"gray"}'}, tvremote: 1b} 1
+kill @s
+function hitchhike:story/intro/start
