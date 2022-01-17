768c9d76132a89fffabf7c217d575fa76f2f2097
UnsignedByte <edlam1889@gmail.com>
2022-01-16 21:35:41 -0800

    b

--- compile-to-mcfunction.ts
+++ compile-to-mcfunction.ts
@@ -894,7 +894,7 @@ export function story(functions: Record<string, Lines>, reset: Lines[], load: Li
     ])
 
     addfunc('intro/tp_remote_holder', [
-      'execute unless entity @e[tag=remote_rightclick_detect] run summon armor_stand ~ ~0.7 ~ {NoGravity:1b,Silent:1b,Invulnerable:1b,ShowArms:1b,Small:1b,Invisible:1b,Tags:["remote_rightclick_detect"],DisabledSlots:4079423}',
+      'execute unless entity @e[tag=remote_rightclick_detect] run summon armor_stand ~ ~0.7 ~ {NoGravity:1b,Silent:1b,Invulnerable:1b,ShowArms:1b,Small:1b,Invisible:1b,Tags:["remote_rightclick_detect"],DisabledSlots:4079422}',
       'tp @e[tag=remote_rightclick_detect] ~ ~0.7 ~'
     ])
   })();
