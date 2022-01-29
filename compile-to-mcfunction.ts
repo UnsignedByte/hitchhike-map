7e77da8f93e184509dbb2f4f6d647c6be68d5f88
UnsignedByte <edlam1889@gmail.com>
2022-01-29 14:47:42 -0800

    b

--- compile-to-mcfunction.ts
+++ compile-to-mcfunction.ts
@@ -4450,10 +4450,10 @@ export function story(files: Record<string, Lines>, functions: Record<string, Li
           wait: 30,
           seq: {
             cmds: [
-              'playsound minecraft:block.piston.contract block @a -207 62 -2 1 1',
-              'playsound minecraft:block.piston.contract block @a -206 62 -2 1 1',
-              'playsound minecraft:block.piston.contract block @a -205 62 -2 1 1',
-              'playsound minecraft:block.piston.contract block @a -204 62 -2 1 1',
+              'playsound minecraft:block.piston.contract block @a -207 62 -2 1 0.8',
+              'playsound minecraft:block.piston.contract block @a -206 62 -2 1 0.8',
+              'playsound minecraft:block.piston.contract block @a -205 62 -2 1 0.8',
+              'playsound minecraft:block.piston.contract block @a -204 62 -2 1 0.8',
               'playsound minecraft:block.stone.break block @a -207 64 -2 1 0.6',
               'playsound minecraft:block.stone.break block @a -206 64 -2 1 0.6',
               'playsound minecraft:block.stone.break block @a -205 64 -2 1 0.6',
@@ -4465,10 +4465,10 @@ export function story(files: Record<string, Lines>, functions: Record<string, Li
                 wait: 30,
                 seq: {
                   cmds: [
-                    'playsound minecraft:block.piston.contract block @a -207 62 -2 1 1',
-                    'playsound minecraft:block.piston.contract block @a -206 62 -2 1 1',
-                    'playsound minecraft:block.piston.contract block @a -205 62 -2 1 1',
-                    'playsound minecraft:block.piston.contract block @a -204 62 -2 1 1',
+                    'playsound minecraft:block.piston.contract block @a -207 62 -2 1 0.8',
+                    'playsound minecraft:block.piston.contract block @a -206 62 -2 1 0.8',
+                    'playsound minecraft:block.piston.contract block @a -205 62 -2 1 0.8',
+                    'playsound minecraft:block.piston.contract block @a -204 62 -2 1 0.8',
                     'playsound minecraft:block.stone.break block @a -207 64 -2 1 0.6',
                     'playsound minecraft:block.stone.break block @a -206 64 -2 1 0.6',
                     'playsound minecraft:block.stone.break block @a -205 64 -2 1 0.6',
@@ -4480,10 +4480,10 @@ export function story(files: Record<string, Lines>, functions: Record<string, Li
                       wait: 30,
                       seq: {
                         cmds: [
-                          'playsound minecraft:block.piston.contract block @a -207 62 -2 1 1',
-                          'playsound minecraft:block.piston.contract block @a -206 62 -2 1 1',
-                          'playsound minecraft:block.piston.contract block @a -205 62 -2 1 1',
-                          'playsound minecraft:block.piston.contract block @a -204 62 -2 1 1',
+                          'playsound minecraft:block.piston.contract block @a -207 62 -2 1 0.8',
+                          'playsound minecraft:block.piston.contract block @a -206 62 -2 1 0.8',
+                          'playsound minecraft:block.piston.contract block @a -205 62 -2 1 0.8',
+                          'playsound minecraft:block.piston.contract block @a -204 62 -2 1 0.8',
                           'playsound minecraft:block.stone.break block @a -207 64 -2 1 0.6',
                           'playsound minecraft:block.stone.break block @a -206 64 -2 1 0.6',
                           'playsound minecraft:block.stone.break block @a -205 64 -2 1 0.6',
@@ -4495,10 +4495,10 @@ export function story(files: Record<string, Lines>, functions: Record<string, Li
                             wait: 30,
                             seq: {
                               cmds: [
-                                'playsound minecraft:block.piston.contract block @a -207 62 -2 1 1',
-                                'playsound minecraft:block.piston.contract block @a -206 62 -2 1 1',
-                                'playsound minecraft:block.piston.contract block @a -205 62 -2 1 1',
-                                'playsound minecraft:block.piston.contract block @a -204 62 -2 1 1',
+                                'playsound minecraft:block.piston.contract block @a -207 62 -2 1 0.8',
+                                'playsound minecraft:block.piston.contract block @a -206 62 -2 1 0.8',
+                                'playsound minecraft:block.piston.contract block @a -205 62 -2 1 0.8',
+                                'playsound minecraft:block.piston.contract block @a -204 62 -2 1 0.8',
                                 'playsound minecraft:block.stone.break block @a -207 64 -2 1 0.6',
                                 'playsound minecraft:block.stone.break block @a -206 64 -2 1 0.6',
                                 'playsound minecraft:block.stone.break block @a -205 64 -2 1 0.6',
@@ -4510,10 +4510,10 @@ export function story(files: Record<string, Lines>, functions: Record<string, Li
                                   wait: 30,
                                   seq: {
                                     cmds: [
-                                      'playsound minecraft:block.piston.contract block @a -207 62 -2 1 1',
-                                      'playsound minecraft:block.piston.contract block @a -206 62 -2 1 1',
-                                      'playsound minecraft:block.piston.contract block @a -205 62 -2 1 1',
-                                      'playsound minecraft:block.piston.contract block @a -204 62 -2 1 1',
+                                      'playsound minecraft:block.piston.contract block @a -207 62 -2 1 0.8',
+                                      'playsound minecraft:block.piston.contract block @a -206 62 -2 1 0.8',
+                                      'playsound minecraft:block.piston.contract block @a -205 62 -2 1 0.8',
+                                      'playsound minecraft:block.piston.contract block @a -204 62 -2 1 0.8',
                                       'playsound minecraft:block.stone.break block @a -207 64 -2 1 0.6',
                                       'playsound minecraft:block.stone.break block @a -206 64 -2 1 0.6',
                                       'playsound minecraft:block.stone.break block @a -205 64 -2 1 0.6',
--- data/generated/functions/story/misc/lobby/opendoor-0-0-0.mcfunction
+++ data/generated/functions/story/misc/lobby/opendoor-0-0-0.mcfunction
@@ -1,7 +1,7 @@
-playsound minecraft:block.piston.contract block @a -207 62 -2 1 1
-playsound minecraft:block.piston.contract block @a -206 62 -2 1 1
-playsound minecraft:block.piston.contract block @a -205 62 -2 1 1
-playsound minecraft:block.piston.contract block @a -204 62 -2 1 1
+playsound minecraft:block.piston.contract block @a -207 62 -2 1 0.8
+playsound minecraft:block.piston.contract block @a -206 62 -2 1 0.8
+playsound minecraft:block.piston.contract block @a -205 62 -2 1 0.8
+playsound minecraft:block.piston.contract block @a -204 62 -2 1 0.8
 playsound minecraft:block.stone.break block @a -207 64 -2 1 0.6
 playsound minecraft:block.stone.break block @a -206 64 -2 1 0.6
 playsound minecraft:block.stone.break block @a -205 64 -2 1 0.6
